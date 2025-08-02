46ac6b993fcc936b7077a96e5dad944c1cd98dfc
Ahmed Amr <ahmedamr4f@gmail.com>
2025-08-02 17:26:03 +0300

    feat: Initialize client and server structure with basic components
    - Implemented App and ErrorPage components in the client application.
    - Set up routing in index.js to handle navigation and error handling.
    - Configured Tailwind CSS for styling in index.css.
    - Established server structure with ASP.NET Core.
    - Created necessary project files and configurations for both client and server.

---
+++ client/src/app/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline bg-blue-500 text-white p-4">
+      Hello world!
+    </h1>
+  );
+};
---
+++ client/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import './index.css';
+
+import { App, ErrorPage } from './app/';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <App /> },
+      // { path: 'path', element: <element /> },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
